4b60f2c0115e0d3d1fcbcfd7a987d1b5dc3bbdbe
insertedscripts <peterpierre.z3@gmail.com>
2016-08-18 13:08:32 +0300

    Add files via upload

---
+++ script1539690.js
@@ -0,0 +1,23 @@
+document.querySelectorAll('.class123-address span:nth-child(1) input')[0].setAttribute('placeholder','Street Address');
+document.querySelectorAll('.class123-address span:nth-child(3) input')[0].setAttribute('placeholder','Street Address Line 2');
+document.querySelectorAll('.class123-city input')[0].setAttribute('placeholder','City');
+document.querySelectorAll('.class123-region input')[0].setAttribute('placeholder','Region');
+document.querySelectorAll('.class123-zip input')[0].setAttribute('placeholder','Postal / Zip Code');
+
+document.querySelectorAll('.class123-address span:nth-child(1) input')[1].setAttribute('placeholder','Street Address');
+document.querySelectorAll('.class123-address span:nth-child(3) input')[1].setAttribute('placeholder','Street Address Line 2');
+document.querySelectorAll('.class123-city input')[1].setAttribute('placeholder','City');
+document.querySelectorAll('.class123-region input')[1].setAttribute('placeholder','Region');
+document.querySelectorAll('.class123-zip input')[1].setAttribute('placeholder','Postal / Zip Code');
+
+document.querySelectorAll('.class123-address span:nth-child(1) input')[2].setAttribute('placeholder','Street Address');
+document.querySelectorAll('.class123-address span:nth-child(3) input')[2].setAttribute('placeholder','Street Address Line 2');
+document.querySelectorAll('.class123-city input')[2].setAttribute('placeholder','City');
+document.querySelectorAll('.class123-region input')[2].setAttribute('placeholder','Region');
+document.querySelectorAll('.class123-zip input')[2].setAttribute('placeholder','Postal / Zip Code');
+
+document.querySelectorAll('.class123-address span:nth-child(1) input')[3].setAttribute('placeholder','Street Address');
+document.querySelectorAll('.class123-address span:nth-child(3) input')[3].setAttribute('placeholder','Street Address Line 2');
+document.querySelectorAll('.class123-city input')[3].setAttribute('placeholder','City');
+document.querySelectorAll('.class123-region input')[3].setAttribute('placeholder','Region');
+document.querySelectorAll('.class123-zip input')[3].setAttribute('placeholder','Postal / Zip Code');
