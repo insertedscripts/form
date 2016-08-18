a306bc3d3b0b4f68a1b2f53d59d154193b614297
insertedscripts <peterpierre.z3@gmail.com>
2016-08-18 14:17:05 +0300

    Add files via upload

---
+++ script_2124268.js
@@ -0,0 +1,4 @@
+document.getElementById("mainform123").onmouseover = function twovaluesinone() {
+var email = document.getElementById("id123-control21003464").value;
+document.getElementById("id123-control21003477").value = email+"_playcore";
+}
