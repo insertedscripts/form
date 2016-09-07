86fb0134588424db600c655ab7c9554a03b8c937
insertedscripts <peterpierre.z3@gmail.com>
2016-09-08 00:01:51 +0300

    Add files via upload

---
+++ script_2179579.js
@@ -0,0 +1,5 @@
+document.getElementById('id123-control21708920').oninput = function emailtwo {
+var em1 = document.getElementById('id123-control21708920').value;
+var em2 = document.getElementById('id123-control21732457').value;
+em2 = em1;
+}
