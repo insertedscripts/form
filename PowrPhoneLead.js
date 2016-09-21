6b852653a01fc928de8006051112a06f30ae937b
insertedscripts <peterpierre.z3@gmail.com>
2016-09-21 20:53:14 +0300

    Add files via upload

---
+++ PowrPhoneLead.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+    cvt = "lead";
+    cvn = "Powr+Phone+Lead";
+    cva = "1.00";
+    guid = "";
+                                            </script> 
+<script type="text/javascript" src="<a href="http://reallyamerican.rurl.me/api/conversion"></script">http://reallyamerican.rurl.me/api/conversion"></script</a>>
+<noscript><img src="<a href="http://reallyamerican.rurl.me/api/pixel/?cvt=lead&cvn=Powr+Phone+Lead&cva=1.00&guid=">http://reallyamerican.rurl.me/api/pixel/?cvt=lead&cvn=Powr+Phone+Lead&cva=1.00&guid=</a>" alt="_" width="1" height="1" border="0" /></noscript>
---
+++ PrePhoneSurvey.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+ cvt="lead";
+ cvn="Powr Pre Phone Sruvey";
+ cva="0.01";
+ guid="";
+</script>
+<script type="text/javascript" src="<a href="http://reallyamerican.rurl.me/api/conversion"></script">http://reallyamerican.rurl.me/api/conversion"></script</a>>
+<noscript><img src="<a href="http://reallyamerican.rurl.me/api/pixel/?cvt=lead&cvn=Powr">http://reallyamerican.rurl.me/api/pixel/?cvt=lead&cvn=Powr</a> Pre Phone Sruvey&cva=0.01&guid=" alt="_" width="1" height="1" border="0" /></noscript>
