eb799ff02ea975ab7c62129de006787d5ab5b6c0
insertedscripts <peterpierre.z3@gmail.com>
2016-11-24 13:40:47 +0200

    Update redirect_customty.js

--- redirect_customty.js
+++ redirect_customty.js
@@ -1,5 +1,5 @@
 window.onload = function redirect_customty() {
 var msg = document.getElementById("error-message").innerHTML;
 var img = document.getElementById("error-img").src;
-if (msg.includes("country")) && (img.src == "/images3/form/form_deactivated.png") {window.location.href="http://www.123contactform.com/thank-you-2287771.html";}
+if (msg.includes("country") && img.src == "/images3/form/form_deactivated.png") {window.location.href="http://www.123contactform.com/thank-you-2287771.html";}
 }
