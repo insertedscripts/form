90c2c6ecfe9cb322ed5d31073177a3c307e4963c
insertedscripts <peterpierre.z3@gmail.com>
2016-11-24 13:47:23 +0200

    Update redirect_customty.js

--- redirect_customty.js
+++ redirect_customty.js
@@ -1,6 +1,6 @@
 window.onload = function redirect_customty() {
 var msg = document.getElementById("error-message").innerHTML;
 var img = document.getElementById("error-img").src;
-if (img.src == "/images3/form/form_deactivated.png") {
+if (img == "/images3/form/form_deactivated.png") {
 	document.getElementById("error-page").style.backgroundColor = "red !important";}
 }
