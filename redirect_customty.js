2198b1fe436a05311a60fc74d57560af9d7b0342
insertedscripts <peterpierre.z3@gmail.com>
2016-11-24 15:06:35 +0200

    Update redirect_customty.js

--- redirect_customty.js
+++ redirect_customty.js
@@ -1,7 +1,8 @@
 window.onload = function redirect_customty() {
-//var msg = document.getElementById("error-message").innerHTML;
+/*var msg = document.getElementById("error-message").innerHTML;
 var img = document.getElementById("error-img").src;
 if (img.includes("form_deactivated.png")) {
 	document.getElementById("error-page").style.backgroundColor = "red";}
-else {document.getElementById("error-page").style.backgroundColor = "brown";}
+else {document.getElementById("error-page").style.backgroundColor = "brown";}*/
+document.getElementById("error-page").style.backgroundColor = "brown"
 }
