51614229ea222276ae721af7650e9d9d45b95abc
insertedscripts <peterpierre.z3@gmail.com>
2016-12-12 23:03:00 +0200

    Update myscript_1685864.js

--- myscript_1685864.js
+++ myscript_1685864.js
@@ -6,6 +6,6 @@ var $subm = $("#myitemlist_myitemlist131630 h2").text();
 var $goal = $("#myitemlist_myitemlist131634 .htmlwidget").text();
 //var goal = document.getElementById("#myitemlist_myitemlist131634").innerText;
 //document.querySelectorAll("#myitemlist_myitemlist132095 .htmlwidget").innerHTML = subm*goal;
-$("#myitemlist_myitemlist132095 .htmlwidget").text($subm/$goal);
+$("#myitemlist_myitemlist132095 .htmlwidget").text($subm);
 //document.getElementById("#myitemlist_myitemlist132095").innerText = subm*goal;
 }
