d2e188b46ef0455b4a8dea696c109101df6470b3
insertedscripts <peterpierre.z3@gmail.com>
2016-12-12 22:09:47 +0200

    Update myscript_1685864.js

--- myscript_1685864.js
+++ myscript_1685864.js
@@ -1,5 +1,6 @@
 window.onload = function percentage() {
-var subm = document.querySelector("#myitemlist_myitemlist131630 h2");
-var goal = document.querySelector("#myitemlist_myitemlist131634 .htmlwidget");
+var subm = document.querySelector("#myitemlist_myitemlist131630 h2.innerHTML");
+//var subm = $( this ).css( "background-color" );
+var goal = document.querySelector("#myitemlist_myitemlist131634 .htmlwidget.innerHTML");
 document.querySelector("#myitemlist_myitemlist131634 .htmlwidget") = subm/goal;
 }
