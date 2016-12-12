c30019351003444c6772938630862b656c7e3432
insertedscripts <peterpierre.z3@gmail.com>
2016-12-12 23:02:02 +0200

    Update myscript_1685864.js

--- myscript_1685864.js
+++ myscript_1685864.js
@@ -1,9 +1,9 @@
 window.onload = function percentage() {
 //var subm = document.querySelectorAll("#myitemlist_myitemlist131630 h2").innerHTML;
-var $subm = $("#myitemlist_myitemlist131630 h2").html();
+var $subm = $("#myitemlist_myitemlist131630 h2").text();
 //var subm = document.getElementById("#myitemlist_myitemlist131630").innerText;
 //var goal = document.querySelectorAll("#myitemlist_myitemlist131634 .htmlwidget").innerHTML;
-var $goal = $("#myitemlist_myitemlist131634 .htmlwidget").html();
+var $goal = $("#myitemlist_myitemlist131634 .htmlwidget").text();
 //var goal = document.getElementById("#myitemlist_myitemlist131634").innerText;
 //document.querySelectorAll("#myitemlist_myitemlist132095 .htmlwidget").innerHTML = subm*goal;
 $("#myitemlist_myitemlist132095 .htmlwidget").text($subm/$goal);
