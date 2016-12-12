f3c686205f41b44117f9eb67751f371c0d88c48e
insertedscripts <peterpierre.z3@gmail.com>
2016-12-12 23:17:14 +0200

    Update myscript_1685864.js

--- myscript_1685864.js
+++ myscript_1685864.js
@@ -1,4 +1,4 @@
-document.getElementById("#AutoNumber1").onmouseover = function percentage() {
+document.getElementById("#sortablewidgets").onmouseover = function percentage() {
 //var subm = document.querySelectorAll("#myitemlist_myitemlist131630 h2").innerHTML;
 var $subm = $("#myitemlist_myitemlist131630 h2").text();
 //var subm = document.getElementById("#myitemlist_myitemlist131630").innerText;
