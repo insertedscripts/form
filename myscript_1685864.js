9905eecaeea5df3a1c5f46d1c58337bbb75801c1
insertedscripts <peterpierre.z3@gmail.com>
2016-12-12 21:53:05 +0200

    Add files via upload

---
+++ myscript_1685864.js
@@ -0,0 +1,5 @@
+window.onload = function percentage() {
+var subm = document.querySelector("#myitemlist_myitemlist131630 h2");
+var goal = document.querySelector("#myitemlist_myitemlist131634 .htmlwidget");
+document.querySelector("#myitemlist_myitemlist131634 .htmlwidget") = subm/goal;
+}
