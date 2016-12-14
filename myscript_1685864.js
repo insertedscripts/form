a0f91a49b2d81467b4db17263939f4ca5898ac3c
insertedscripts <peterpierre.z3@gmail.com>
2016-12-14 20:13:05 +0200

    Update myscript_1685864.js

--- myscript_1685864.js
+++ myscript_1685864.js
@@ -1,12 +1,12 @@
 window.onload = function percentage() {
 //var subm = document.querySelectorAll("#myitemlist_myitemlist131630 h2").innerHTML;
-var $subm = $("#myitemlist_myitemlist131630 h2").text();
-//var subm = document.getElementById("#myitemlist_myitemlist131630").innerText;
+//var $subm = $("#myitemlist_myitemlist131630 h2").text();
+var subm = document.getElementById("myitemlist_myitemlist131630").innerText;
 //var goal = document.querySelectorAll("#myitemlist_myitemlist131634 .htmlwidget").innerHTML;
-var $goal = $("#myitemlist_myitemlist131634 .widgettitle span:first").text();
-//var goal = document.getElementById("#myitemlist_myitemlist131634").innerText;
+//var $goal = $("#myitemlist_myitemlist131634 .widgettitle span:first").text();
+var goal = document.getElementById("myitemlist_myitemlist131634").innerText;
 //document.querySelectorAll("#myitemlist_myitemlist132095 .htmlwidget").innerHTML = subm*goal;
 //$("#myitemlist_myitemlist132095 .widgettitle span:first").text($goal);
-  document.write($goal)
-//document.getElementById("#myitemlist_myitemlist132095").innerText = subm*goal;
+//document.write(goal)
+document.getElementById("myitemlist_myitemlist132095").innerText = subm*goal;
 }
