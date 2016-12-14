79d07357a1a3f8e6a5ec5ed7391b581a78772e30
insertedscripts <peterpierre.z3@gmail.com>
2016-12-14 21:30:39 +0200

    Update myscript_1685864.js

--- myscript_1685864.js
+++ myscript_1685864.js
@@ -1,12 +1,12 @@
 //window.onload
 document.getElementById("header-text").onmouseover = function percentage() {
-var subm = document.querySelectorAll("#myitemlist_myitemlist132083 h2").textContent;
+var subm = document.querySelectorAll("#myitemlist_myitemlist132083 h2").innerText;
 //var $subm = $("#myitemlist_myitemlist132083 h2").text();
 //var subm = document.getElementById("myitemlist_myitemlist132083").innerHTML;
-var goal = document.querySelectorAll("#myitemlist_myitemlist132084 .widgettitle > span").textContent;
+var goal = document.querySelectorAll("#myitemlist_myitemlist132084 .widgettitle > span").innerText;
 //var $goal = $("#myitemlist_myitemlist132084 .widgettitle > span").text();
 //var goal = document.getElementById("myitemlist_myitemlist132084").innerHTML;
-document.querySelectorAll("#myitemlist_myitemlist132095 .widgettitle > span.htmlwidget").textContent = subm;
+document.querySelectorAll("#myitemlist_myitemlist132095 .widgettitle > span.htmlwidget").innerHTML = subm;
 //$("#myitemlist_myitemlist132095 .widgettitle > span.htmlwidget").text($subm/$goal);
 //document.write("a"+subm+"b")
 //document.getElementById("myitemlist_myitemlist132095").innerHTML = subm*goal;
