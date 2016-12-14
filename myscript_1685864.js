47a922587840b8989a8b4871e6a863ed660caaf6
insertedscripts <peterpierre.z3@gmail.com>
2016-12-14 20:07:06 +0200

    Update myscript_1685864.js

--- myscript_1685864.js
+++ myscript_1685864.js
@@ -6,6 +6,7 @@ var $subm = $("#myitemlist_myitemlist131630 h2").text();
 var $goal = $("#myitemlist_myitemlist131634 .widgettitle span:first").text();
 //var goal = document.getElementById("#myitemlist_myitemlist131634").innerText;
 //document.querySelectorAll("#myitemlist_myitemlist132095 .htmlwidget").innerHTML = subm*goal;
-$("#myitemlist_myitemlist132095 .widgettitle span:first").text($goal);
+//$("#myitemlist_myitemlist132095 .widgettitle span:first").text($goal);
+  document.write('*Hello, there!*')
 //document.getElementById("#myitemlist_myitemlist132095").innerText = subm*goal;
 }
