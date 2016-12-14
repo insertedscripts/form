0a8fb4fd1cdf9a92c8621f96f2223617a630869a
insertedscripts <peterpierre.z3@gmail.com>
2016-12-14 21:10:43 +0200

    Update myscript_1685864.js

--- myscript_1685864.js
+++ myscript_1685864.js
@@ -1,10 +1,10 @@
 //window.onload
 document.getElementById("header-text").onmouseover = function percentage() {
-var subm = document.querySelectorAll("#myitemlist_myitemlist131630 h2").textContent;
-//var $subm = $("#myitemlist_myitemlist131630 h2").text();
+//var subm = document.querySelectorAll("#myitemlist_myitemlist131630 h2").textContent;
+var $subm = $("#myitemlist_myitemlist131630 h2").text();
 //var subm = document.getElementById("myitemlist_myitemlist131630").innerHTML;
-var goal = document.querySelectorAll("#myitemlist_myitemlist131634 .widgettitle > span").textContent;
-//var $goal = $("#myitemlist_myitemlist131634 .widgettitle span:first").text();
+//var goal = document.querySelectorAll("#myitemlist_myitemlist131634 .widgettitle > span").textContent;
+var $goal = $("#myitemlist_myitemlist131634 .widgettitle > span").text();
 //var goal = document.getElementById("myitemlist_myitemlist131634").innerHTML;
 //document.querySelectorAll("#myitemlist_myitemlist132095 .widgettitle > span").innerHTML = subm;
 //$("#myitemlist_myitemlist132095 .widgettitle span:first").text($goal);
