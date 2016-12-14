4ef70b0922fd7de088a5633196417de6007104bc
insertedscripts <peterpierre.z3@gmail.com>
2016-12-14 22:21:59 +0200

    Update myscript_1685864.js

--- myscript_1685864.js
+++ myscript_1685864.js
@@ -1,5 +1,5 @@
 window.onload = function percentage() {
 var $subm = $("#myitemlist_myitemlist132083 h2").text();
 var $goal = $("#myitemlist_myitemlist132084 .widgettitle > span.htmlwidget").text();
-$("#myitemlist_myitemlist132095 .widgettitle > span.htmlwidget").text($subm/$goal);
+$("#myitemlist_myitemlist132095 .widgettitle > span.htmlwidget").text($subm);
 }
