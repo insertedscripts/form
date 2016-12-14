21dc26fce07bfc2368736657978781949a258bd3
insertedscripts <peterpierre.z3@gmail.com>
2016-12-14 20:48:07 +0200

    Update myscript_1685864.js

--- myscript_1685864.js
+++ myscript_1685864.js
@@ -8,6 +8,6 @@ var goal = document.querySelectorAll("#myitemlist_myitemlist131634 .widgettitle 
 //var goal = document.getElementById("myitemlist_myitemlist131634").innerHTML;
 //document.querySelectorAll("#myitemlist_myitemlist132095 .widgettitle > span").innerHTML = subm;
 //$("#myitemlist_myitemlist132095 .widgettitle span:first").text($goal);
-document.write(subm)
+document.write("a"+subm+"b")
 //document.getElementById("myitemlist_myitemlist132095").innerHTML = subm*goal;
 }
