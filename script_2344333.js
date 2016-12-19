46f5fa3088c4f0c865b4e01b5dc171971068a1c0
insertedscripts <peterpierre.z3@gmail.com>
2016-12-20 01:13:37 +0200

    Update script_2344333.js

--- script_2344333.js
+++ script_2344333.js
@@ -2,5 +2,5 @@ window.onload = function percentage() {
 var $subm0 = $("#myitemlist_myitemlist131318 h2").text();
 var $subm = $subm0.match(/[0-9]{1,}/);
 var $goal = $("#myitemlist_myitemlist132390 .widgettitle > span.htmlwidget").text();
-$("#myitemlist_myitemlist132391 .widgettitle > span.htmlwidget").text($subm/$goal*100);
+$("#myitemlist_myitemlist132391 .widgettitle > span.htmlwidget").text($subm/$goal*100+"%");
 }
