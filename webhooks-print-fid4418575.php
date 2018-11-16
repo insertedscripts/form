ef01123f4089f732180983be5d92fc5ba5afb446
insertedscripts <peterpierre.z3@gmail.com>
2018-11-16 13:06:36 +0200

    Update webhooks-print-fid4418575.php

--- webhooks-print-fid4418575.php
+++ webhooks-print-fid4418575.php
@@ -1,18 +1,5 @@
 <?php
 
-/*
-$formdata = ARRAY();
-foreach ($_POST as $key => $value) {
-$value = stripslashes($value);
-$formdata[$key]=$value;
-}
-if (array_key_exists('uid', $formdata)==false) die('One essential parameter was not POST-ed. Exiting.');
-
-$printtxt="Received data:\r\n";
-
-$printtxt.="$formdata[controlname47504547] is "; $printtxt.="$formdata[controlvalue47504547]\r\n";
-*/
-
 $deprintat = DOMDocument::getElementsByTagName("input")[1];
 
 echo $deprintat;
