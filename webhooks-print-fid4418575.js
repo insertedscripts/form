d701227ad953cd341ec77c56f7f5ef2b5aa10a7b
insertedscripts <peterpierre.z3@gmail.com>
2018-11-16 15:02:16 +0200

    Update webhooks-print-fid4418575.js

--- webhooks-print-fid4418575.js
+++ webhooks-print-fid4418575.js
@@ -1,4 +1,4 @@
-document.getElementById("form-heading-00000002-acc").onclick = function myFunction() {
+document.getElementById("text-00000008-acc").onclick = function myFunction() {
 var deprintat = document.getElementById("text-00000008-acc").innerHTML;
 document.write(deprintat);
 }
