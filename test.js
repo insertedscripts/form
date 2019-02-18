a7464f21fbd8a308a226373400df1f480bea8bd2
insertedscripts <peterpierre.z3@gmail.com>
2019-02-18 14:53:15 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,5 @@
-console.log("loaded test.js");
-$$(document).ready(function() {
+window.$$(document).ready(function() {
   $("[data-role=choice]").click(function() {
-      console.log("CLICKED TEST");
      var selectedEl = $( this );
      selectedEl.siblings( "[data-selected=1]" ).removeAttr("data-selected");
      selectedEl.attr("data-selected", "1");
