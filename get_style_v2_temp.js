d311bdbf4a97c7fd7440b995401ecf2f23162f74
insertedscripts <peterpierre.z3@gmail.com>
2019-03-06 23:51:22 +0200

    Update get_style_v2_temp.js

--- get_style_v2_temp.js
+++ get_style_v2_temp.js
@@ -1,11 +1,43 @@
+<style>
+
+
+		document.onmouseover = function mousepos(event) {
+		
+			setTimeout(function() {
+			var y_pos_200 = event.clientY;
+			console.log("position of hover 200ms: " + y_pos_200);
+			}, 200);
+			
+			setTimeout(function() {
+			var y_pos_2000 = event.clientY;
+			console.log("position of hover 2000ms: " + y_pos_2000);
+			}, 2000);
+		
+		}
+	
+	
 
 document.onmouseover = function myCSS(event) {
-	//var declaration = document.styleSheets[0].cssRules[0].style;
-		//var allfields = document.querySelector("div")[0];
-	//var allfields = document.getElementsByTagName("div");
-	//var clr = event.target.getAttribute("class");
 	var font_size = getComputedStyle(event.target).getPropertyValue("font-size");
 	var color = getComputedStyle(event.target).getPropertyValue("color");
+	
+	
+	/*var rgbToHex = function(rgb) { 
+	var hex = Number(rgb).toString(16);
+	if (hex.length < 2) {
+		hex = "0" + hex;
+		}
+	return hex;
+	};
+	
+	var fullColorHex = function(r,g,b) {   
+	var red = rgbToHex(r);
+	var green = rgbToHex(g);
+	var blue = rgbToHex(b);
+	return red+green+blue;
+	};*/
+	
+	
 	var font_family = getComputedStyle(event.target).getPropertyValue("font-family");
 	var background_color = getComputedStyle(event.target).getPropertyValue("background-color");
 	var height = getComputedStyle(event.target).getPropertyValue("height");
@@ -16,10 +48,18 @@ document.onmouseover = function myCSS(event) {
 	//var top_left = getBoundingClientRect(event.target);
 	//var placeholder = $(event.target).attr( "placeholder" );
 	var z_index = getComputedStyle(event.target).getPropertyValue("z-index");
-	event.target.style.outline = "1px solid red";
+	
+	//event.target.style.outline = "1px solid red";
+	
 	//setPropertyValue("color");
 	//$(event.target).css('outline','1px solid red');
-	console.log("------------------------" +
+		
+
+
+			setTimeout(function() {
+				
+			event.target.style.outline = "1px solid red";
+			console.log("------------------------" +
 		"\n" +
 		"\n" +
 		"font-size: " + font_size + "\n" +
@@ -45,7 +85,10 @@ document.onmouseover = function myCSS(event) {
 		"\n" +
 		"\n" +
 		"------------------------");
-};
+			}, 3000);
+	
+	};
+	
 
 
-
+</style>
