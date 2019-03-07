467d6d8eef720e918ea9a6206f97b8f1545612b3
insertedscripts <peterpierre.z3@gmail.com>
2019-03-07 23:54:05 +0200

    Update get_style_v2_temp.js

--- get_style_v2_temp.js
+++ get_style_v2_temp.js
@@ -1,41 +1,65 @@
 
+var pos_div200 = document.createElement('div');
+pos_div200.setAttribute("id", "div200");
+pos_div200.style.border = "1px solid red";
+pos_div200.style.color = "red";
+pos_div200.innerHTML = "position 1";
+document.body.appendChild(pos_div200);
 
-		document.onmouseover = function mousepos(event) {
+var pos_div2000 = document.createElement('div');
+pos_div2000.setAttribute("id", "div2000");
+pos_div2000.style.border = "1px solid red";
+pos_div2000.style.color = "red";
+pos_div2000.innerHTML = "position 2";
+document.body.appendChild(pos_div2000);
+
+var pos_divdiff = document.createElement('div');
+pos_divdiff.setAttribute("id", "divdiff");
+pos_divdiff.style.border = "1px solid red";
+pos_divdiff.style.color = "red";
+pos_divdiff.innerHTML = "difference between positions";
+document.body.appendChild(pos_divdiff);
+
+
+
+document.onmouseover = function get_style_mouse_stops(event) {
 		
-			setTimeout(function() {
-			var y_pos_200 = event.clientY;
-			console.log("position of hover 200ms: " + y_pos_200);
-			}, 200);
-			
-			setTimeout(function() {
-			var y_pos_2000 = event.clientY;
-			console.log("position of hover 2000ms: " + y_pos_2000);
-			}, 2000);
+	setTimeout(function() {
+	var x_pos_200 = event.clientX;
+	var y_pos_200 = event.clientY;
+	pos_div200.innerHTML = x_pos_200 + ", " + y_pos_200;
+	}, 200);
+	
+	setTimeout(function() {
+	var x_pos_2000 = event.clientX;
+	var y_pos_2000 = event.clientY;
+	pos_div2000.innerHTML = x_pos_2000 + ", " + y_pos_2000;
+	}, 2000);
+	
+	
+	
+	document.onmousemove = setTimeout(function() {
 		
+	var pos_div200_value = pos_div200.innerHTML;
+	var pos_div2000_value = pos_div2000.innerHTML;
+
+	if (pos_div200_value == pos_div2000_value) {
+		console.log("mouse is not moving");
+		pos_divdiff.innerHTML = 0;
 		}
-	
-	
+	if (pos_div200_value !== pos_div2000_value) {
+		console.log("mouse is moving");
+		pos_divdiff.innerHTML = 1;
+		}
 
-document.onmouseover = function myCSS(event) {
+	}, 2500);
+
+
+
+	setTimeout(function() {
+	
 	var font_size = getComputedStyle(event.target).getPropertyValue("font-size");
-	var color = getComputedStyle(event.target).getPropertyValue("color");
-	
-	
-	/*var rgbToHex = function(rgb) { 
-	var hex = Number(rgb).toString(16);
-	if (hex.length < 2) {
-		hex = "0" + hex;
-		}
-	return hex;
-	};
-	
-	var fullColorHex = function(r,g,b) {   
-	var red = rgbToHex(r);
-	var green = rgbToHex(g);
-	var blue = rgbToHex(b);
-	return red+green+blue;
-	};*/
-	
+	var color = getComputedStyle(event.target).getPropertyValue("color");	
 	
 	var font_family = getComputedStyle(event.target).getPropertyValue("font-family");
 	var background_color = getComputedStyle(event.target).getPropertyValue("background-color");
@@ -44,21 +68,16 @@ document.onmouseover = function myCSS(event) {
 	var padding = getComputedStyle(event.target).getPropertyValue("padding");
 	var border = getComputedStyle(event.target).getPropertyValue("border");
 	var margin = getComputedStyle(event.target).getPropertyValue("margin");
-	//var top_left = getBoundingClientRect(event.target);
-	//var placeholder = $(event.target).attr( "placeholder" );
+
 	var z_index = getComputedStyle(event.target).getPropertyValue("z-index");
-	
-	//event.target.style.outline = "1px solid red";
-	
-	//setPropertyValue("color");
-	//$(event.target).css('outline','1px solid red');
 		
-
-
-			setTimeout(function() {
-				
-			event.target.style.outline = "1px solid red";
-			console.log("------------------------" +
+	var pos_divdiff_value = pos_divdiff.innerHTML;
+		
+		
+	if (pos_divdiff_value == 0) {
+		event.target.style.outline = "1px solid red";
+		console.clear();
+		console.log("------------------------" +
 		"\n" +
 		"\n" +
 		"font-size: " + font_size + "\n" +
@@ -74,17 +93,19 @@ document.onmouseover = function myCSS(event) {
 		"border: " + border + "\n" +
 		"margin: " + margin + "\n" +
 		"\n" +
-		//"top & left: " + top_left.top + " & " + top_left.left + "\n" +
-		//"bottom left: " + bottom_left + "\n" +
-		//"top right: " + top_right + "\n" +
-		//"bottom right: " + bottom_right + "\n" +
-		"\n" +
-		//"placeholder: " + placeholder + "\n" +
 		"z-index: " +
 		"\n" +
 		"\n" +
 		"------------------------");
-			}, 3000);
+		};
+			
+	if (pos_divdiff_value == 1) {
+		console.clear();
+		console.log("mouse is moving");
+	};	
 	
-	};
-	
+	}, 3000);
+
+
+};
+
