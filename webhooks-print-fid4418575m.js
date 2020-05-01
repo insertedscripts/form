a80c35d5e37e3c0387b803f5e0e1640697d85dd7
insertedscripts <peterpierre.z3@gmail.com>
2020-05-01 21:46:02 +0300

    Delete webhooks-print-fid4418575m.js

--- webhooks-print-fid4418575m.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("form-heading-00000002-acc").onclick = function myFunction() {
-var deprintat = document.getElementById("text-00000008-acc").innerHTML;
-window.print(deprintat);
-}
